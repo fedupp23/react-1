cc6b6675b5e51dacf4c6f5a47cf602371ab98781
krish <141345008+fedupp23@users.noreply.github.com>
2024-08-04 19:30:14 +0530

    done some chnges

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const heading=React.createElement('h1',{},"hwllo from react");
-const root=ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+//jsx
+const heading=(<h1 id="heading">
+    namaste reacrusing jsx
+</h1>
+);
+//React component
+//class based component-OLD way of writing components
+//functional component-NEW way of writing components
+
+//react fucntional component
+
+const HeadinComponent=()=> {
+<div id="root1">
+<h1>Namaste from functional component</h1>
+</div>
+};
+const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+root.render(<HeadinComponent/>); //this is  how  you render a functional component inside react
