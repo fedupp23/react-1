6aa5b889035ef72f2dd1e483f0f193b2ca2d6c58
krish <141345008+fedupp23@users.noreply.github.com>
2024-08-05 17:03:07 +0530

    basic chnges

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
-
-//jsx
-const heading=(<h1 id="heading">
-    namaste reacrusing jsx
-</h1>
-);
 //React component
 //class based component-OLD way of writing components
 //functional component-NEW way of writing components
 
 //react fucntional component
 
-const HeadinComponent=()=> {
-<div id="root1">
-<h1>Namaste from functional component</h1>
-</div>
+const Header=()=>{
+    return(
+         <div className='header'>
+            <div className='Logo-Container'>
+                <img className="logo" src='https://w7.pngwing.com/pngs/664/210/png-transparent-uber-eats-muncheez-delivery-online-food-ordering-food-delivery-food-logo-uber-eats.png' />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
+         </div>
+    )
 };
+const styleCard={   //js object
+    backgroundColor:"#f0f0f0"
+};
+const RestaurantCard=()=>{
+    return(
+        <div className='res-card' style={styleCard}>
+            <h3>MegnaFoods</h3>
+        </div>
+    )
+}
+const Body=()=>{
+    return(
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+const AppLayout=()=>{
+    return(
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-root.render(<HeadinComponent/>); //this is  how  you render a functional component inside react
+root.render(<AppLayout />); //this is  how  you render a functional component inside react
