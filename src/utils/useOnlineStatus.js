768ee970f1827bf62d02cb7c88e9f557ebdea97f
krish <141345008+fedupp23@users.noreply.github.com>
2024-08-16 23:40:06 +0530

    done

--- src/utils/useOnlineStatus.js
+++ src/utils/useOnlineStatus.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const useOnlineStatus=()=>{
 
-    const [onlineStatus,setOnlineStatus]=useState(true);
+    const [onlineStatus,setOnlineStatus]=useState();
      //check if online
     useEffect(()=>{
         window.addEventListener("online",()=>{
